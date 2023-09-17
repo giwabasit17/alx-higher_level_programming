8a2231ca6fcc5c98a1a0a4f76e500cc77dcf2910
giwabasit17 <giwabasit17@gmail.com>
2023-09-17 06:56:41 -0700

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Module that containes the class definition of City and instance Base"""
-from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""
+Defines a City model.
+Inherits from SQLAlchemy Base and links to the MySQL table cities.
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """City class that inherits from Base"""
-    __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True, unique=True,
-                nullable=False, primary_key=True)
+    """Represents a city for a MySQL database.
+    Attributes:
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
